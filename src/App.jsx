f339363b958ea68f7dac4f54994a1898acaab248
Zaki-Moha <zakimohaze@gmail.com>
2025-06-02 08:53:51 +0300

    Pages Creating Part Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Shop from "./Pages/Shop";
+import Product from "./Pages/Product";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Carts from "./Pages/Carts";
+import Checkout from "./Pages/Checkout";
+import Error from "./Pages/Error";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/shop" element={<Shop />}></Route>
+      <Route path="/product" element={<Product />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/carts" element={<Carts />}></Route>
+      <Route path="/checkout" element={<Checkout />}></Route>
+      <Route path="/*" element={<Error />}></Route>
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <div>
-      <h1>This is Me</h1>
-    </div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
